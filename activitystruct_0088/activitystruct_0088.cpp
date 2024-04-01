056bcb69e2799f0325d116354a7772f1ec7d3ba6
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-04-01 15:09:35 +0700

    mengubah  cin mhs.alamat.desa

--- activitystruct_0088/activitystruct_0088.cpp
+++ activitystruct_0088/activitystruct_0088.cpp
@@ -42,8 +42,28 @@ struct DetailAlamat {
 struct Mahasiswa {
 	string nim;
 	string nama;
-	string alamat;
+	DetailAlamat alamat;
 	int umur;
 };
 
+int main() {
+	Mahasiswa mhs;
+	cout << "nomor Mahasiswa :";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cout << "\t Nama Desa :";
+	cin >> mhs.alamat.desa;
+	cout << "Umur Mahasiswa: ";
+	cin >> mhs.umur;
+
+	cout << endl;
+
+	cout << "\n NIM: " << mhs.nim;
+	cout << "\n Nama: " << mhs.nama;
+	cout << "\n Alamat: " << mhs.alamat;
+	cout << "\n Umur: " << mhs.umur;
+
+}
 
