a6f7661ec97176dadd3fb69efe7e86c6fff6e8e3
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-04-01 14:44:21 +0700

    cin mhs alamat

--- activitystruct_0088/activitystruct_0088.cpp
+++ activitystruct_0088/activitystruct_0088.cpp
@@ -13,4 +13,11 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs;
 	cout << "nomor Mahasiswa :";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+
+
 }
