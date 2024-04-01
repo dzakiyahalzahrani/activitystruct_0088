2c4a18b3c2f34826bdf2a25d8d1efd124f43d7ce
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2024-04-01 15:11:31 +0700

    menambahkan cout nama kota

--- activitystruct_0088/activitystruct_0088.cpp
+++ activitystruct_0088/activitystruct_0088.cpp
@@ -55,6 +55,8 @@ int main() {
 	cout << "Alamat Mahasiswa : ";
 	cout << "\t Nama Desa :";
 	cin >> mhs.alamat.desa;
+	cout << "\t Nama Kota";
+	cin >> mhs.alamat.kota;
 	cout << "Umur Mahasiswa: ";
 	cin >> mhs.umur;
 
